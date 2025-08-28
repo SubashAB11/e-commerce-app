acec3b9d16dcdd02efed7ba600876aa825984647
SubossAB <subash.a.b123@gmail.com>
2025-08-28 19:11:08 +0530

    added jwks controller for exposing public key

--- services/auth/src/main/java/com/abs/auth/security/JWTService.java
+++ services/auth/src/main/java/com/abs/auth/security/JWTService.java
@@ -4,6 +4,7 @@ import com.abs.auth.auth.AuthenticationResponse;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@Getter
 public class JWTService {
 
     private static final String TOKEN_TYPE = "token_type";
