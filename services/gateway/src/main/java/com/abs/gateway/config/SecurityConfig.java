1fbbd0ab53c6c3c62638a3a39967c37afc2c9064
SubossAB <subash.a.b123@gmail.com>
2025-08-29 19:46:36 +0530

    added gateway security

---
+++ services/gateway/src/main/java/com/abs/gateway/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.abs.gateway.config;
+
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import java.util.Objects;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
+
+                        .pathMatchers(HttpMethod.POST, "/api/v1/auth/**").permitAll()
+                        .anyExchange().authenticated())
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .oauth2ResourceServer(jwt -> jwt.jwt(Customizer.withDefaults()))
+                .build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder reactiveJwtDecoder(ReactiveDiscoveryClient discoveryClient) {
+        String uri = Objects.requireNonNull(discoveryClient.getInstances("AUTH-SERVICE")
+                        .blockFirst())
+                .getUri()
+                + "/.well-known/jwks.json";
+        return NimbusReactiveJwtDecoder.withJwkSetUri(uri).build();
+    }
+
+
+}
